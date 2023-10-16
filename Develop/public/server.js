ac26162871f32495266ae42cda909848ea8ead9c
Kyle Oros <kyleblissoros@gmail.com>
2023-10-16 03:08:11 -0700

    changes to server.js

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -1,17 +1,13 @@
 const express = require('express');
 const fs = require('fs');
-
 const app = express();
 
 const PORT = 3001;
 
 const dbFile = require('../db/db.json');
 
-app.listen(PORT, () => {
-    console.log(`App listening at http://localhost:${PORT}`)
-});
-
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const read = () => {
     const notes = fs.readFileSync(dbFile, 'utf-8');
@@ -20,13 +16,21 @@ const read = () => {
 
 const write = (notes) => {
     const jsonData = JSON.stringify(notes);
-    fs.writeFileSync(dbFile, jsonData)
+    fs.writeFileSync(db, jsonData)
 }
 
-app.get('../db/db.json', (req, res) => {
+app.get('/notes', (req, res) => {
     const notes = read();
     res.json(notes);
 });
+
+app.get('*', (req, res) => {
+    res.send('<a href="/">Go back home?</a>');
+})
+
+app.get('/api/notes', (req, res) => {
+    res.json(dbFile);
+})
 
 app.post('../db/db.json', (req, res) => {
     const notes = read();
@@ -36,6 +40,7 @@ app.post('../db/db.json', (req, res) => {
     res.json(notes);
 });
 
+/*
 app.delete('../db/db.json:id', (req, res) => {
     const notes = read();
     const noteID = req.params.id;
@@ -44,5 +49,8 @@ app.delete('../db/db.json:id', (req, res) => {
     write(notes);
     res.json(notes);
 })
+*/
 
-module.exports = app;
+app.listen(PORT, () => {
+    console.log(`App listening at http://localhost:${PORT}`)
+});
